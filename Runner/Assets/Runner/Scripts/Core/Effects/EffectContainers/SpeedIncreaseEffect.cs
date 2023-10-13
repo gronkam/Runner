7e27e396e90e3c0655b259944e5798a8e211f26a
gronkam <gronkam@gmail.com>
2023-10-13 20:20:46 +0200

    add new speed increase effect

---
+++ Runner/Assets/Runner/Scripts/Core/Effects/EffectContainers/SpeedIncreaseEffect.cs
@@ -0,0 +1,15 @@
+﻿namespace Runner.Core
+{
+    public class SpeedIncreaseEffect : BaseEffect<SpeedIncreaseEffectSettings>
+    {
+        public SpeedIncreaseEffect(SpeedIncreaseEffectSettings settings, ITime time) : 
+            base(settings, time)
+        {
+        }
+
+        public override void ApplyEffect(IEffectable effectable)
+        {
+            effectable.Speed += Settings.SpeedIncreaseAmount;
+        }
+    }
+}
---
+++ Runner/Assets/Runner/Scripts/Core/Effects/Effects/SpeedIncreaseEffectSettings.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Runner.Core
+{
+    [CreateAssetMenu(fileName = "SpeedIncrease", menuName = "Configs/Effects/SpeedIncrease")]
+    public class SpeedIncreaseEffectSettings : BaseEffectSettings<SpeedIncreaseEffect>
+    {
+        [field: SerializeField]
+        public float SpeedIncreaseAmount { get; private set; }
+    }
+}
